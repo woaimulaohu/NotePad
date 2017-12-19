5be12858004e1414bd22344d66080e275295b9ec
tuzki <11761740@qq.com>
2017-12-19 22:21:34 +0800

    1

---
+++ MyNotePad/Util/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.UI.Xaml.Controls.Primitives;
+
+namespace MyNotePad.Util
+{
+    public static class Helper
+    {
+        public class PageHelper
+        {
+            public static RadBusyIndicator radBusyIndicator;
+            public static void LoadingAnimationSwitch(bool isShow)
+            {
+                radBusyIndicator.IsActive = isShow;
+            }
+        }
+    }
+}
